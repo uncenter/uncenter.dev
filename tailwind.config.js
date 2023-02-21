e216c697c78526567a1cfd669ede49adb109b78d
uncenter <me@uncenter.org>
2023-02-21 11:14:05 -0500

    feat: add Typography plugin, custom link styles, nord colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('tailwind-nord'),
-  ],
-}
+    content: ["./dist/**/*.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("tailwind-nord"), require("@tailwindcss/typography")],
+};
