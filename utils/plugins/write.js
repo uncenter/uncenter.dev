e74cb1aa1663eac13703aa2f494dee7c15170738
uncenter <me@uncenter.org>
2023-03-23 18:52:25 -0400

    refactor: consolidate two scripts

--- utils/plugins/write.js
+++ utils/plugins/write.js
@@ -1,19 +1,46 @@
 #!/usr/bin/env node
 
-const luxon = require('luxon');
 const fs = require('fs');
 const path = require('path');
 const lodash = require('lodash');
+const luxon = require('luxon');
 const Chalk = require('chalk');
 
-const utils = require('./utils.js');
+// from blakeembrey/change-case
+// https://github.com/blakeembrey/change-case/blob/master/packages/title-case/src/index.ts
+// license: MIT
+function titleCase(input) {
+    const SMALL_WORDS = /\b(?:an?d?|a[st]|because|but|by|en|for|i[fn]|neither|nor|o[fnr]|only|over|per|so|some|tha[tn]|the|to|up|upon|vs?\.?|versus|via|when|with|without|yet)\b/i;
+    const TOKENS = /[^\s:–—-]+|./g;
+    const WHITESPACE = /\s/;
+    const IS_MANUAL_CASE = /.(?=[A-Z]|\..)/;
+    const ALPHANUMERIC_PATTERN = /[A-Za-z0-9\u00C0-\u00FF]/;
+    var result = "";
+    var m;
+    while ((m = TOKENS.exec(input)) !== null) {
+        var token = m[0], index = m.index;
+        if (!IS_MANUAL_CASE.test(token) &&
+            (!SMALL_WORDS.test(token) ||
+                index === 0 ||
+                index + token.length === input.length) &&
+            (input.charAt(index + token.length) !== ":" ||
+                WHITESPACE.test(input.charAt(index + token.length + 1)))) {
+            result += token.replace(ALPHANUMERIC_PATTERN, function (m) {
+                return m.toUpperCase();
+            });
+            continue;
+        }
+        result += token;
+    }
+    return result;
+}
 
 let title = process.argv.slice(2).join(' ');
 if (title === undefined || title.trim() === '') {
     console.log(Chalk.red('Please provide a title for the post.'));
     process.exit(1);
 } else {
-    title = utils.titleCase(title);
+    title = titleCase(title);
 }
 date = luxon.DateTime.local().toFormat('yyyy-MM-dd'); // 2023-03-05
 
