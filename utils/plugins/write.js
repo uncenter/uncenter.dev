4ba65923e8baa5c89abed4e4ac095f61f4628e2f
uncenter <me@uncenter.org>
2023-03-24 15:58:44 -0400

    feat: better npm run write

--- utils/plugins/write.js
+++ utils/plugins/write.js
@@ -4,7 +4,19 @@ const fs = require('fs');
 const path = require('path');
 const lodash = require('lodash');
 const luxon = require('luxon');
+const inquirer = require('inquirer');
 const Chalk = require('chalk');
+
+function fileExists(filePath) {
+    const existingFiles = fs.readdirSync(path.join(__dirname, '../../src/content/posts'));
+    for (let i = 0; i < existingFiles.length; i++) {
+        if (filePath === existingFiles[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
 
 // from blakeembrey/change-case
 // https://github.com/blakeembrey/change-case/blob/master/packages/title-case/src/index.ts
@@ -35,34 +47,77 @@ function titleCase(input) {
     return result;
 }
 
-let title = process.argv.slice(2).join(' ');
-if (title === undefined || title.trim() === '') {
-    console.log(Chalk.red('Please provide a title for the post.'));
-    process.exit(1);
-} else {
-    title = titleCase(title);
-}
-date = luxon.DateTime.local().toFormat('yyyy-MM-dd'); // 2023-03-05
-
-content = `---
-tags: []
-title: ${title}
-date: ${date}
-description: ...
-# cspell:ignore ...
----
-
-Voluptate cupidatat ut consequat sunt sint consequat eu duis deserunt velit deserunt culpa ea.
-`;
-
-const filename = `${lodash.kebabCase(title)}.md`;
-const filepath = path.join(__dirname, '../../src/content/posts', filename);
-const existingFiles = fs.readdirSync(path.join(__dirname, '../../src/content/posts'));
-for (let i = 0; i < existingFiles.length; i++) {
-    if (filename === existingFiles[i]) {
-        console.log(Chalk.red(`File already exists: ${Chalk.dim(filename)}`));
-        process.exit(1);
+async function run() {
+    let title = process.argv.slice(2).join(' ');
+    if (title === undefined || title.trim() === '') {
+        const answer = await inquirer.prompt({
+            type: 'input',
+            name: 'title',
+            message: 'Enter a title for the post:',
+            validate: function (value) {
+                if (value.trim() === '') {
+                    return 'Please provide a title.';
+                }
+                let filename = lodash.kebabCase(value) + '.md';
+                if (fileExists(filename)) {
+                    return Chalk.red(`File already exists: `) + Chalk.underline(filename);
+                }
+                return true;
+            }
+        });
+        title = titleCase(answer.title);
+    } else {
+        title = titleCase(title);
     }
+    date = luxon.DateTime.local().toFormat('yyyy-MM-dd'); // 2023-03-05
+
+    tags = [];
+    keywords = [
+        { '11ty': 'Eleventy' }, // <<< = value (alternate name, not a tag)
+        //    ^^^^ = key (tag)
+        'JavaScript',
+        'Umami',
+        'Python',
+        'Tailwind'
+    ];
+    for (let word of title.split(' ')) {
+        word = word.toLowerCase();
+        for (let keyword of keywords) {
+            if (typeof keyword === 'string') {
+                if (word === keyword.toLowerCase()) {
+                    tags.push(keyword);
+                }
+            } else {
+                for (let [key, value] of Object.entries(keyword)) {
+                    if (word === key.toLowerCase() || word === value.toLowerCase()) {
+                        tags.push(key);
+                    }
+                }
+            }
+        }
+    }
+    tagString = "";
+    for (let tag of tags) {
+        tagString += `'${tag}', `;
+    }
+    tagString = tagString.slice(0, -2);
+
+    const filename = `${lodash.kebabCase(title)}.md`;
+    const filepath = path.join(__dirname, '../../src/content/posts', filename);
+
+    content = `---
+    tags: [${tagString}]
+    title: ${title}
+    date: ${date}
+    description: ...
+    # cspell:ignore ...
+    ---
+
+    Voluptate cupidatat ut consequat sunt sint consequat eu duis deserunt velit deserunt culpa ea.
+    `;
+
+    fs.writeFileSync(filepath, content);
+    console.log(Chalk.green(`✔ Success!`) + ` Created ${Chalk.underline(filename)} (${Chalk.underline(filepath)})`);
 }
-fs.writeFileSync(filepath, content);
-console.log(Chalk.green(`Created file: ${Chalk.dim(filename)}`));
+
+run();
