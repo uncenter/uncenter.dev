248506d59bec0330f9d754db1db405c9a38510d2
uncenter <me@uncenter.org>
2023-03-29 00:51:54 -0400

    feat: fully switch to eleventy-plugin-icons by clearing out the src/_assets/icons dir!

--- eleventy.config.js
+++ eleventy.config.js
@@ -100,14 +100,16 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(pluginShikier);
     eleventyConfig.addPlugin(pluginRSS);
     eleventyConfig.addPlugin(pluginIcons, {
-        mode: 'sprite',
+        mode: 'inline',
         sources: {
-            custom: "./src/_assets/icons",
+            devicon: "./src/_assets/icons/devicon",
+            social: "./src/_assets/icons/social",
         },
-        default: "custom",
+        enable: ["lucide", "devicon", "social"],
+        default: "lucide",
         insertSpriteSheet: {
             shortcode: "iconSheet",
-        }
+        },
     });
 
     /* Layouts */
