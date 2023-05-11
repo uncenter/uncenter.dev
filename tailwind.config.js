b43b298fd0b0065f0abdbf7f5b71d150e51afc26
uncenter <me@uncenter.org>
2023-05-11 10:09:42 -0400

    feat: Berenis ADF Pro fonts, blank icons, base styling, no tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,3 @@
-const disabled = {
-	'code::before': false,
-	'code::after': false,
-	pre: false,
-	code: false,
-	'pre code': false,
-	'h1 code': false,
-	'h2 code': false,
-	'h3 code': false,
-	'h4 code': false,
-	'h5 code': false,
-	'h6 code': false,
-	'a code': false,
-	'pre code': false,
-	'code::before': false,
-	'code::after': false,
-	a: false,
-};
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{11ty.js,js,md,njk,html}', './utils/**/*.js'],
@@ -27,7 +8,6 @@ module.exports = {
 			bg: 'var(--bg)',
 			fg: 'var(--fg)',
 			accent: 'var(--accent)',
-			surface: 'var(--surface)',
 		},
 		extend: {
 			fontFamily: {
@@ -38,15 +18,7 @@ module.exports = {
 					'Roboto',
 					'Helvetica Neue',
 				],
-				serif: ['ui-serif', 'Georgia'],
-				display: ['"Inter"', 'sans-serif'],
-			},
-			typography: {
-				DEFAULT: { css: disabled },
-				sm: { css: disabled },
-				lg: { css: disabled },
-				xl: { css: disabled },
-				'2xl': { css: disabled },
+				serif: ['"Berenis ADF Pro"', 'Georgia', 'Cambria'],
 			},
 			lineHeight: {
 				none: 'unset',
@@ -54,7 +26,8 @@ module.exports = {
 		},
 	},
 	corePlugins: {
+		preflight: false,
 		container: false,
 	},
-	plugins: [require('@tailwindcss/typography')],
+	plugins: [],
 };
