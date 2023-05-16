24338d921e67f9aa076592653ec5e7a8b283c9ba
uncenter <me@uncenter.org>
2023-05-15 20:32:09 -0400

    refactor: optimize copy.js - optimized SVGs, less LOC

--- src/_assets/scripts/copy.js
+++ src/_assets/scripts/copy.js
@@ -1,16 +1,16 @@
-const ICON_CLIPBOARD_COPY = `<svg aria-hidden="true" focusable="false" fill=none height=24 stroke=currentColor stroke-linecap=round stroke-linejoin=round stroke-width=2 viewBox="0 0 24 24"width=24 xmlns=http://www.w3.org/2000/svg><rect height=4 rx=1 ry=1 width=8 x=8 y=2></rect><path d="M8 4H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2"></path><path d="M16 4h2a2 2 0 0 1 2 2v4"></path><path d="M21 14H11"></path><path d="m15 10-4 4 4 4"></path></svg>`;
-const ICON_CLIPBOARD_CHECK = `<svg aria-hidden="true" focusable="false" fill=none height=24 stroke=currentColor stroke-linecap=round stroke-linejoin=round stroke-width=2 viewBox="0 0 24 24"width=24 xmlns=http://www.w3.org/2000/svg><rect height=4 rx=1 ry=1 width=8 x=8 y=2></rect><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><path d="m9 14 2 2 4-4"></path></svg>`;
+const ICON_COPY = `<svg aria-hidden="true" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><rect height="4" rx="1" ry="1" width="8" x="8" y="2"/><path d="M8 4H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2M16 4h2a2 2 0 0 1 2 2v4m1 4H11"/><path d="m15 10-4 4 4 4"/></svg>`;
+const ICON_CHECK = `<svg aria-hidden="true" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><rect height="4" rx="1" ry="1" width="8" x="8" y="2"/><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/><path d="m9 14 2 2 4-4"/></svg>`;
 
 document.querySelectorAll('pre.shiki').forEach((block) => {
-	const copyButton = document.createElement('button');
-	copyButton.innerHTML = ICON_CLIPBOARD_COPY;
+	const button = document.createElement('button');
+	button.innerHTML = ICON_COPY;
 
-	copyButton.addEventListener('click', () => {
+	button.addEventListener('click', () => {
 		navigator.clipboard.writeText(block.querySelector('code').textContent);
-		copyButton.innerHTML = ICON_CLIPBOARD_CHECK;
+		button.innerHTML = ICON_CHECK;
 		setTimeout(() => {
-			copyButton.innerHTML = ICON_CLIPBOARD_COPY;
+			button.innerHTML = ICON_COPY;
 		}, 1500);
 	});
-	block.querySelector('.language-id').appendChild(copyButton);
+	block.querySelector('.language-id').appendChild(button);
 });
