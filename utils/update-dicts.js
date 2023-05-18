5c9664fce04fe6c0bf3c0376f2702530173489d1
uncenter <me@uncenter.org>
2023-05-18 15:17:53 -0400

    build: make sure json response is correct

--- utils/update-dicts.js
+++ utils/update-dicts.js
@@ -9,11 +9,17 @@ function getRepos() {
 	const reposURL = 'https://api.github.com/users/uncenter/repos';
 
 	(async () => {
-		const repos = await fetch(reposURL)
-			.then((res) => res.json())
-			.then((json) => json.map((repo) => repo.name));
+		const response = await fetch(reposURL);
+		const json = await response.json();
 
-		fs.writeFileSync(reposFile, repos.join('\n'));
+		console.log('Response:', json);
+
+		if (Array.isArray(json)) {
+			const repos = json.map((repo) => repo.name);
+			fs.writeFileSync(reposFile, repos.join('\n'));
+		} else {
+			console.log('Invalid response format:', json);
+		}
 	})();
 }
 
