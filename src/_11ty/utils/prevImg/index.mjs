5c3df73e8521429fa3673bd9e8a31d1a6530ec07
uncenter <me@uncenter.org>
2023-05-22 10:09:20 -0400

    feat: add preview images (credit to @ryanccn)

---
+++ src/_11ty/utils/prevImg/index.mjs
@@ -0,0 +1,205 @@
+// Via: https://github.com/ryanccn/ryanccn.dev/tree/main/src/utils/socialImages
+// License: MIT
+
+import satori from 'satori';
+
+import { fileURLToPath } from 'node:url';
+import { join } from 'node:path';
+import { mkdir, readFile, rm } from 'node:fs/promises';
+
+import { format } from 'date-fns';
+import kleur from 'kleur';
+import pLimit from 'p-limit';
+import sharp from 'sharp';
+
+const startTime = performance.now();
+
+const FONTS_DIR = join(fileURLToPath(new URL('.', import.meta.url)), 'fonts');
+const FONTS = [
+	{
+		name: 'Inter',
+		weight: 400,
+		style: 'normal',
+		data: await readFile(join(FONTS_DIR, 'Inter-Regular.otf')),
+	},
+	{
+		name: 'Inter',
+		weight: 400,
+		style: 'italic',
+		data: await readFile(join(FONTS_DIR, 'Inter-Italic.otf')),
+	},
+	{
+		name: 'Inter',
+		weight: 500,
+		style: 'normal',
+		data: await readFile(join(FONTS_DIR, 'Inter-Medium.otf')),
+	},
+	{
+		name: 'Inter',
+		weight: 500,
+		style: 'italic',
+		data: await readFile(join(FONTS_DIR, 'Inter-MediumItalic.otf')),
+	},
+	{
+		name: 'Inter',
+		weight: 600,
+		style: 'normal',
+		data: await readFile(join(FONTS_DIR, 'Inter-SemiBold.otf')),
+	},
+	{
+		name: 'Inter',
+		weight: 600,
+		style: 'italic',
+		data: await readFile(join(FONTS_DIR, 'Inter-SemiBoldItalic.otf')),
+	},
+	{
+		name: 'Inter',
+		weight: 700,
+		style: 'normal',
+		data: await readFile(join(FONTS_DIR, 'Inter-Bold.otf')),
+	},
+	{
+		name: 'Inter',
+		weight: 700,
+		style: 'italic',
+		data: await readFile(join(FONTS_DIR, 'Inter-BoldItalic.otf')),
+	},
+	{
+		name: 'Satoshi',
+		weight: 800,
+		style: 'normal',
+		data: await readFile(join(FONTS_DIR, 'Satoshi-Black.otf')),
+	},
+	{
+		name: 'Satoshi',
+		weight: 800,
+		style: 'italic',
+		data: await readFile(join(FONTS_DIR, 'Satoshi-Black.otf')),
+	},
+];
+
+const makeImage = async (data) => {
+	console.log(
+		`[11ty][${kleur.magenta('previews')}] ${kleur.bold('dist/previews/')}${kleur
+			.cyan()
+			.bold(data.slug)}${kleur.bold('.png')}`,
+	);
+
+	try {
+		const result = await satori(
+			{
+				type: 'div',
+				props: {
+					children: [
+						{
+							type: 'h1',
+							props: {
+								children: data.title,
+								style: {
+									fontFamily: 'Satoshi',
+									fontWeight: 800,
+									fontSize: '80px',
+									textWrap: 'balance',
+									width: '100%',
+								},
+							},
+						},
+						{
+							type: 'div',
+							props: {
+								style: { display: 'flex', flexDirection: 'column' },
+								children: [
+									{
+										type: 'p',
+										props: {
+											children: [
+												{
+													type: 'span',
+													props: {
+														children: 'uncenter',
+														style: { fontWeight: 700, marginRight: '0.625rem' },
+													},
+												},
+												{
+													type: 'span',
+													props: { children: '· uncenter.org' },
+												},
+											],
+											style: {
+												fontFamily: 'Inter',
+												fontWeight: 500,
+												fontSize: '30px',
+												display: 'flex',
+												alignItems: 'baseline',
+											},
+										},
+									},
+									...(data.date
+										? [
+												{
+													type: 'p',
+													props: {
+														children: format(new Date(data.date), 'yyyy/MM/dd'),
+														style: {
+															fontFamily: 'Inter',
+															fontWeight: 400,
+															color: '#a3a3a3',
+															fontSize: '30px',
+														},
+													},
+												},
+										  ]
+										: []),
+								],
+							},
+						},
+					],
+					style: {
+						height: '100vh',
+						width: '100vw',
+						padding: '4rem',
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'space-between',
+						alignItems: 'flex-start',
+						backgroundColor: '#171717',
+						color: '#ffffff',
+					},
+				},
+			},
+			{
+				width: 1200,
+				height: 630,
+				fonts: FONTS,
+			},
+		);
+
+		await sharp(Buffer.from(result))
+			.png()
+			.toFile(`dist/previews/${data.slug}.png`);
+	} catch (err) {
+		console.error(
+			kleur.red(`[11ty][previews] Error generating preview for ${data.slug}`),
+		);
+		throw err;
+	}
+};
+
+const pagesData = await readFile('./pages.json').then(JSON.parse);
+const lim = pLimit(10);
+
+await rm('dist/previews', { force: true, recursive: true });
+await mkdir('dist/previews', { recursive: true });
+
+await Promise.all(pagesData.map((data) => lim(() => makeImage(data))));
+
+console.log(
+	kleur.green(
+		`[11ty] Wrote ${pagesData.length} previews in ${(
+			performance.now() - startTime
+		).toFixed(2)}ms (${(
+			(performance.now() - startTime) /
+			pagesData.length
+		).toFixed(2)}ms each)`,
+	),
+);
