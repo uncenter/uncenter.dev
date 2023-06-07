a2a3ca60e9bb469d8be91defdb7ee56f5cfda14e
uncenter <me@uncenter.org>
2023-06-07 10:34:39 -0400

    refactor: use recommended code block language class pattern

--- utils/plugins/shikier.renderer.js
+++ utils/plugins/shikier.renderer.js
@@ -74,7 +74,7 @@ function renderToHtml(lines, options = {}) {
 	return h(
 		'pre',
 		{
-			className: 'shiki ' + (`lang-${options.lang}` || ''),
+			className: 'shiki ' + (`language-${options.lang}` || ''),
 			style: `background-color: ${bg}`,
 		},
 		[
