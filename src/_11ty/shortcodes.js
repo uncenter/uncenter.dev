0e58a23fd22e7989556924531811d09c68e4fcac
uncenter <47499684+uncenter@users.noreply.github.com>
2023-06-25 15:04:02 -0400

    refactor: remove unused shortcodes

--- src/_11ty/shortcodes.js
+++ src/_11ty/shortcodes.js
@@ -1,103 +1,14 @@
-const turndown = require('turndown');
 const fs = require('fs/promises');
 
 const Image = require('@11ty/eleventy-img');
 const imageSize = require('image-size');
 
 const { escape } = require('lodash');
-const { parseHTML } = require('linkedom');
 const { markdownLibrary } = require('../../utils/plugins/markdown');
 
-const getReadingTime = require('./utils/readingTime.js');
-const cleanContent = require('./utils/cleanContent.js');
-const wordCount = require('./utils/wordCount.js');
 const stringifyAttributes = require('./utils/stringifyAttributes.js');
 const log = require('./utils/log.js');
 const kleur = require('kleur');
-
-const getExcerpt = (page, classes = '') => {
-	function addClassToFirstParagraph(html, classes) {
-		const { document } = parseHTML(html);
-		document.firstChild.classList.add(classes);
-		return document.documentElement.outerHTML;
-	}
-	if (!page.content) return null;
-	if (page.excerpt)
-		return addClassToFirstParagraph(
-			markdownLibrary.render(page.excerpt),
-			classes,
-		);
-
-	let content = page.content
-		.trim()
-		.replace(/^(\s*\n*)?<h\d[^>]*>.*?<\/h\d>(\s*\n*)?/i, '');
-
-	const nextHeadingIndex = content.search(/<h\d[^>]*>/i);
-	if (nextHeadingIndex !== -1) {
-		content = content.substring(0, nextHeadingIndex);
-	}
-
-	const turndownRenderer = new turndown();
-	plainText = turndownRenderer.turndown(content);
-
-	// Via: https://github.com/mpcsh/eleventy-plugin-description
-	// Split on terminal punctuation followed by whitespace (phrases).
-	const phrases = plainText.split(
-		/(\p{Terminal_Punctuation}\p{White_Space})/gu,
-	);
-	// Concatenate phrases until character limit (190) is reached.
-	let excerpt = '';
-	while (phrases.length > 0 && excerpt.length < 190) {
-		excerpt += phrases.shift();
-	}
-
-	excerpt += '...';
-	return addClassToFirstParagraph(markdownLibrary.render(excerpt), classes);
-};
-
-const getCollectionWordCount = (posts) => {
-	if (!posts) return 0;
-	let words = 0;
-	posts.forEach((post) => {
-		words += parseInt(wordCount(cleanContent(post.content)));
-	});
-	return words;
-};
-
-const getCollectionReadingTime = (posts) => {
-	if (!posts) return 0;
-	let readingTime = 0;
-	posts.forEach((post) => {
-		readingTime += parseInt(getReadingTime(post.content));
-	});
-	return readingTime;
-};
-
-const getCollectionWordCountAverage = (posts) => {
-	if (!posts) return 0;
-	let words = 0;
-	posts.forEach((post) => {
-		words += wordCount(cleanContent(post.content));
-	});
-	return Math.round(words / posts.length);
-};
-
-const getCollectionAverageWordLength = (posts) => {
-	if (!posts) return 0;
-
-	const averageWordLengths = [];
-	posts.forEach((post) => {
-		const count = wordCount(cleanContent(post.content));
-		const contentLength = cleanContent(post.content).length;
-		const averageWordLength = Math.round(contentLength / count);
-		averageWordLengths.push(averageWordLength);
-	});
-	return averageWordLengths && averageWordLengths.length > 0
-		? Math.round(
-				averageWordLengths.reduce((a, b) => a + b) / averageWordLengths.length,
-		  )
-		: 0;
-};
 
 const createCallout = (content, title, type) => {
 	const titleText =
@@ -182,17 +93,6 @@ const insertImage = async function (src, alt, width, height) {
 };
 
 module.exports = (eleventyConfig) => {
-	eleventyConfig.addShortcode('excerpt', getExcerpt);
-	eleventyConfig.addShortcode('totalWordCount', getCollectionWordCount);
-	eleventyConfig.addShortcode('totalReadingTime', getCollectionReadingTime);
-	eleventyConfig.addShortcode(
-		'wordCountAverage',
-		getCollectionWordCountAverage,
-	);
-	eleventyConfig.addShortcode(
-		'wordLengthAverage',
-		getCollectionAverageWordLength,
-	);
 	eleventyConfig.addPairedShortcode('note', (content, title) => {
 		return createCallout(content, title, 'note');
 	});
