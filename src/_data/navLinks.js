10bd5efff943edbeeea6283acf46baf3fd1f8b16
uncenter <47499684+uncenter@users.noreply.github.com>
2023-06-30 23:53:32 +0100

    refactor: insert footer links directly, refactor header nav links

---
+++ src/_data/navLinks.js
@@ -0,0 +1,18 @@
+module.exports = [
+	{
+		title: 'Articles',
+		url: '/',
+	},
+	{
+		title: 'Projects',
+		url: '/projects/',
+	},
+	{
+		title: 'Uses',
+		url: '/uses/',
+	},
+	{
+		title: 'Colophon',
+		url: '/colophon/',
+	},
+];
