5593ff3e473d3eaffe355a3284d315c4b632d505
uncenter <47499684+uncenter@users.noreply.github.com>
2023-07-11 15:35:25 +0800

    refactor: remove unused filters

--- src/_11ty/utils.filters.js
+++ src/_11ty/utils.filters.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-
 const { DateTime } = require('luxon');
 const { markdownLibrary } = require('../../utils/plugins/markdown');
 
@@ -39,17 +37,6 @@ const toFullDateTime = (dateObj) => {
 	);
 };
 
-const setAttr = (content, attribute, value = false) => {
-	const regex = new RegExp(`${attribute}=".*?"`, 'g');
-	let el = content.match(/<[^>]*>/)[0];
-	if (el.match(regex)) {
-		el = el.replace(regex, value ? `${attribute}="${value}"` : '');
-	} else {
-		el = el.replace('>', ` ${attribute}="${value}">`);
-	}
-	return content.replace(/<[^>]*>/, el);
-};
-
 module.exports = (eleventyConfig) => {
 	eleventyConfig.addFilter('toShortDate', toShortDate);
 	eleventyConfig.addFilter('toMedDate', toMedDate);
@@ -67,14 +54,6 @@ module.exports = (eleventyConfig) => {
 	eleventyConfig.addFilter('toHtmlInline', (content) => {
 		return markdownLibrary.renderInline(content);
 	});
-	eleventyConfig.addFilter('setAttr', setAttr);
-	eleventyConfig.addFilter('readFile', (filePath) => {
-		const fileContents = fs.readFileSync(filePath, (err, data) => {
-			if (err) throw err;
-			return data;
-		});
-		return fileContents.toString('utf8');
-	});
 	eleventyConfig.addFilter('ordinal', (number) => {
 		const i = number % 10,
 			j = number % 100;
@@ -91,11 +70,5 @@ module.exports = (eleventyConfig) => {
 	});
 	eleventyConfig.addFilter('capitalize', (str) => {
 		return str.charAt(0).toUpperCase() + str.slice(1);
-	});
-	eleventyConfig.addFilter('head', (iter, x = 1) => {
-		return iter.slice(0, x);
-	});
-	eleventyConfig.addFilter('tail', (iter, x = 1) => {
-		return iter.slice(x);
 	});
 };
