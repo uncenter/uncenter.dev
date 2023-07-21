537a1536beaa390ad1f3f088fe523d708a565e43
uncenter <47499684+uncenter@users.noreply.github.com>
2023-07-21 06:10:26 -0400

    revert: "refactor: less dom manipulation on page load by using transforms during build"

--- src/_11ty/transforms.js
+++ src/_11ty/transforms.js
@@ -15,7 +15,7 @@ module.exports = {
 	wrapShiki: (content) => {
 		const { document } = parseHTML(content);
 		document.querySelectorAll('pre.shiki').forEach((block, index) => {
-			const code = block.querySelector('code').textContent;
+			const code = block.querySelector('code').innerHTML;
 			const genUniqueId = (content) => {
 				return require('crypto')
 					.createHash('md5')
@@ -26,32 +26,7 @@ module.exports = {
 			while (document.getElementById(uniqueId)) {
 				uniqueId = genUniqueId(uniqueId).slice(0, 6);
 			}
-			const wrapper = document.createElement('div');
-			wrapper.classList.add('shiki-wrapper');
-			wrapper.setAttribute('id', uniqueId);
-			block.parentNode.replaceChild(wrapper, block);
-			wrapper.appendChild(block);
-
-			const codeToolbar = document.createElement('div');
-			codeToolbar.classList.add(
-				'code-toolbar',
-				'absolute',
-				'right-0',
-				'top-0',
-				'flex',
-				'gap-1',
-				'hidden',
-			);
-			codeToolbar.setAttribute('aria-hidden', 'true');
-			const copyButton = document.createElement('button');
-			copyButton.classList.add('copy-button');
-			copyButton.setAttribute('aria-label', 'Copy code to clipboard');
-			const linkButton = document.createElement('button');
-			linkButton.classList.add('link-button');
-			linkButton.setAttribute('aria-label', 'Go to code block');
-			codeToolbar.appendChild(linkButton);
-			codeToolbar.appendChild(copyButton);
-			wrapper.appendChild(codeToolbar);
+			block.outerHTML = `<div class="shiki-wrapper" id="${uniqueId}">${block.outerHTML}</div>`;
 		});
 		return document.documentElement.outerHTML;
 	},
