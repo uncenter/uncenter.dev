e7593a006854eea742908cf93c07ce7d031a9771
uncenter <47499684+uncenter@users.noreply.github.com>
2023-07-29 15:06:48 -0400

    fix: analytics domain and links

--- src/_data/analytics.js
+++ src/_data/analytics.js
@@ -1,10 +1,10 @@
 const analytics = {
-	url: 'https://stats.uncenter.org',
+	url: 'https://analytik.uncenter.dev',
 	websiteId: 'dea82084-7eb8-4337-b02c-23f6ace1afc1',
 };
 
 module.exports = {
 	...analytics,
 	script: `${analytics.url}/script.js`,
-	share: `${analytics.url}/share/LRerfjMi/uncenter`,
+	share: `${analytics.url}/share/Atf7LPtQCPGa0643/uncenter`,
 };
