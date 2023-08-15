ab844ebd9592522cd65f2dfa5b6acadd39aa831f
uncenter <47499684+uncenter@users.noreply.github.com>
2023-08-15 18:49:42 +0200

    fix(data): use custom url field

--- src/_data/domain.js
+++ src/_data/domain.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: require('../../package.json').name,
 	url:
 		process.env.NODE_ENV === 'production'
-			? require('../../package.json').author.url
+			? require('../../package.json').url
 			: 'http://localhost:8080/',
 };
