a5777026fad0ba46057ac45e6bcf40a017a1394c
uncenter <47499684+uncenter@users.noreply.github.com>
2023-09-17 18:23:04 -0400

    fix: give posts type: 'content'

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,6 +2,7 @@ import { defineCollection, z } from 'astro:content';
 
 export const collections = {
 	posts: defineCollection({
+		type: 'content',
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
