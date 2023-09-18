e82963e1303a1f84d2da1ba1c3c2de4f18cf6ce1
uncenter <47499684+uncenter@users.noreply.github.com>
2023-09-18 09:31:51 -0400

    fix: remove broken rehype plugins

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
-// import rehypeShikiji from 'rehype-shikiji';
-// import rehypeSlug from 'rehype-slug';
-// import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-// import rehypeToc from 'rehype-toc';
 import unpluginIcons from 'unplugin-icons/vite';
 
 import { url } from './src/data/site';
@@ -20,17 +16,6 @@ export default defineConfig({
 			gfm: true,
 		}),
 	],
-	markdown: {
-		remarkPlugins: [],
-		rehypePlugins: [
-			// rehypeSlug(),
-			// rehypeAutolinkHeadings({ behavior: 'prepend' }),
-			// rehypeToc({ headings: ['h2', 'h3', 'h4'] }),
-			// rehypeShikiji({
-			// 	themes: { light: 'github-light', dark: 'github-dark' },
-			// }),
-		],
-	},
 	vite: {
 		plugins: [
 			unpluginIcons({
