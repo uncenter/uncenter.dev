1ea8d66707412eeaaaa51beb2097d3e56b33600e
uncenter <47499684+uncenter@users.noreply.github.com>
2023-10-20 10:52:44 -0400

    feat: update styles and layout to v6

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,6 +1,9 @@
-export default {
-	Articles: '/posts/',
-	Projects: '/projects/',
-	Uses: '/uses/',
-	Privacy: '/privacy/',
+const header = { Articles: '/posts/', Projects: '/projects/', Uses: '/uses/' };
+
+const footer = {
+	'RSS Feed': '/feed.xml',
+	'JSON Feed': '/feed.json',
+	'Privacy & Licensing': '/privacy/',
+	Sitemap: '/sitemap.xml',
 };
+export default { header, footer };
