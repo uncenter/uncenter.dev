a048147c12b4d31823ab300596fdbb384ca90604
uncenter <47499684+uncenter@users.noreply.github.com>
2024-01-04 07:32:11 -0500

    refactor: keep views disable but add space for views in listings

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -4,22 +4,10 @@ require('dotenv').config();
 const { DateTime } = require('luxon');
 const getViewsForPage = require('../../utils/umami');
 
-// const { z } = require('zod');
-
-// const schema = z.object({
-// 	title: z.string(),
-// 	description: z.string(),
-// 	tags: z.array(z.string()),
-// 	published: z.date(),
-// 	edited: z.optional(z.date()),
-// 	comments: z.optional(z.boolean()).default(true),
-// });
-
 module.exports = {
 	eleventyComputed: {
 		views: async (data) => {
-			if (isDevelopment || process.env.UMAMI_SKIP)
-				return Math.floor(Math.random() * 100);
+			if (isDevelopment || process.env.UMAMI_SKIP) return;
 
 			return getViewsForPage(data.page.url, data.page.date);
 		},
