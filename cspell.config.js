dcf43f951e305cafc785ce728894140b0a95104e
uncenter <uncenter@uncenter.dev>
2025-10-31 14:56:28 -0500

    style: formatting

--- cspell.config.js
+++ cspell.config.js
@@ -1,7 +1,16 @@
 export default {
 	version: '0.2',
 	language: 'en',
-	words: ['uncenter', 'eleventy', '11ty', 'umami', 'shortcodes', 'keycaps', 'userstyles', 'catppuccin'],
+	words: [
+		'uncenter',
+		'eleventy',
+		'11ty',
+		'umami',
+		'shortcodes',
+		'keycaps',
+		'userstyles',
+		'catppuccin',
+	],
 	flagWords: [],
 	dictionaries: [],
 	dictionaryDefinitions: [],
