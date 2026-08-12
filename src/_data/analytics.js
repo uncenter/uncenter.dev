11d5e006bc7aa714d0191b773baa3c03114c13ab
uncenter <uncenter@uncenter.dev>
2026-08-12 12:05:32 -0400

    feat: switch analytics over to new origami instance

--- src/_data/analytics.js
+++ src/_data/analytics.js
@@ -1,10 +1,11 @@
 const analytics = {
-	url: 'https://analytik.uncenter.dev',
+	url: 'https://origami.uncenter.dev',
 	websiteId: 'dea82084-7eb8-4337-b02c-23f6ace1afc1',
+	shareId: 'Atf7LPtQCPGa0643',
 };
 
 export default {
 	...analytics,
 	script: `${analytics.url}/script.js`,
-	share: `${analytics.url}/share/Atf7LPtQCPGa0643/uncenter`,
+	share: `${analytics.url}/share/${analytics.shareId}`,
 };
